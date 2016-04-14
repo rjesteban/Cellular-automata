66f93f311dbb9df82b4a5f0b0669d1799c7a6afe
Arnold Joseph Esteban <ajcp.esteban@gmail.com>
2016-04-15 00:51:43 +0800

    Update mp3.py

--- mp3.py
+++ mp3.py
@@ -3,6 +3,11 @@ iterations = int(raw_input('iterations >>'))
 
 x_old = ['0']*(n + 2)
 x = ['0']*(n + 2)
+
+# seed = raw_input('enter seed:')
+# for i in range(1, n):
+#    x[i] = seed[i]
+
 if n % 2 == 0:
     x[(n / 2)] = '1'
 else:
@@ -21,10 +26,10 @@ for j in range(iterations):
     for l in range(len(x_old)):
         x_old[l] = x[l]
     for i in range(1, n + 1):
-        a = x_old[i - 1] == '0' and x_old[i] == '0' and x_old[i + 1] == '1'
-        b = x_old[i - 1] == '0' and x_old[i] == '1' and x_old[i + 1] == '0'
-        c = x_old[i - 1] == '0' and x_old[i] == '1' and x_old[i + 1] == '1'
-        d = x_old[i - 1] == '1' and x_old[i] == '0' and x_old[i + 1] == '0'
+        a = ''.join(x_old[i - 1: i + 2]) == '001'
+        b = ''.join(x_old[i - 1: i + 2]) == '010'
+        c = ''.join(x_old[i - 1: i + 2]) == '011'
+        d = ''.join(x_old[i - 1: i + 2]) == '100'
         if a or b or c or d:
             x[i] = '1'
         else:
